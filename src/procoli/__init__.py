f3ae78da203b719b512e0399203e761ad236bfef
Tanvi Karwal <karwal@sas.upenn.edu>
2023-12-13 21:40:12 -0600

    removed importlib from init file, version must be manually changed now

--- src/procoli/__init__.py
+++ src/procoli/__init__.py
@@ -1,6 +1,7 @@
 # read version from installed package
-from importlib.metadata import version
-__version__ = version(__name__)
+# from importlib.metadata import version
+# __version__ = version(__name__)
+__version__ = '0.0.1'
 
 # populate package namespace
 from procoli.mp_procoli_functions import lkl_prof
