f154084bf58e9a9bc8bda790b7a5e7e75fae5df7
Tanvi Karwal <33636657+tkarwal@users.noreply.github.com>
2024-01-24 23:12:04 -0500

    Bumped version number for release

--- src/procoli/__init__.py
+++ src/procoli/__init__.py
@@ -1,7 +1,7 @@
 # read version from installed package
 # from importlib.metadata import version
 # __version__ = version(__name__)
-__version__ = '0.0.1'
+__version__ = '1.0.0'
 
 # populate package namespace
 from procoli.mp_procoli_functions import lkl_prof
